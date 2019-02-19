fabfb55428a3ef4af5dcd28be29bf4c5455dea1b
Brendan Armani <brendanarmani@gmail.com>
2019-02-19 13:29:12 -0500

    Updates to dependencies, started styling the login page

--- p2-angular/src/app/login/login.component.ts
+++ p2-angular/src/app/login/login.component.ts
@@ -5,7 +5,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+  login: boolean = true;
   constructor() { }
 
   ngOnInit() {
