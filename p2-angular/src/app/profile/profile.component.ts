28f1e230ca9e93e1d809c143c97558a293130f3e
Brendan Armani <brendanarmani@gmail.com>
2019-02-19 23:13:25 -0500

    Added hiding profile functionality; for now, ta's ar are odd ids and students are even ids in the url

--- p2-angular/src/app/profile/profile.component.ts
+++ p2-angular/src/app/profile/profile.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarComponent } from '../calendar/calendar.component';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -7,10 +8,17 @@ import { CalendarComponent } from '../calendar/calendar.component';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  id: number = 0;
+  showStudent: boolean = false;
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    this.id = parseInt(this.route.snapshot.paramMap.get('id'));
+    console.log(this.id);
+    this.showStudent = this.id % 2 == 0;
   }
 
 }
