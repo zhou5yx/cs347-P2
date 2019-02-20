b34d2c7675f05373824235e011c0b9197741e72a
Brendan Armani <brendanarmani@gmail.com>
2019-02-20 17:27:14 -0500

    Added initial calendar week view

--- p2-angular/src/app/calendar/calendar.component.ts
+++ p2-angular/src/app/calendar/calendar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-
+  weekView: boolean = false;
   constructor() { }
 
   ngOnInit() {
