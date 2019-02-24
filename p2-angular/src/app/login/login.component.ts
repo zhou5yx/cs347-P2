889d0044498cbe61d4e31f982c175287c3dd098d
Brendan Armani <brendanarmani@gmail.com>
2019-02-23 19:06:58 -0500

    Login form routes to pages on submission

--- p2-angular/src/app/login/login.component.ts
+++ p2-angular/src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,13 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   login: boolean = true;
-  constructor() { }
+
+  constructor (
+    private router: Router
+  ) {}
 
   ngOnInit() {
   }
 
   onSubmit(form) {
-    console.log(form);
+    console.log(form.toggle_option);
+    if (form.toggle_option === "ta") {
+      this.router.navigate(['/profile/1']);
+    } else if (form.toggle_option === "student") {
+      this.router.navigate(['/profile/2']);
+    } else if (form.toggle_option === "admin"){
+      this.router.navigate(['/session']);
+    }
   }
 
 }
