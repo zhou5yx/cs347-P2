6a3f90b0bb7fe0bd930193b0a9ef97602a2c0231
zhou5yx <zhou5yx@dukes.jmu.edu>
2019-02-25 22:15:42 -0500

    removed add a ta from admin, ta management only display if semester is selected

--- p2-angular/src/app/admin/admin.component.ts
+++ p2-angular/src/app/admin/admin.component.ts
@@ -2,6 +2,41 @@ import { Component, OnInit,ViewChild } from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import {MatSort, MatTableDataSource} from '@angular/material';
 import {SelectionModel} from '@angular/cdk/collections';
+import {MatMenuModule} from '@angular/material/menu';
+
+export interface FakeData {
+  Requester: string;
+  Requestee: string;
+  Course: string;
+  Shift: string;
+  Status: string;
+  description: string;
+}
+
+const FAKE_DATA: FakeData[] = [
+  {
+    Requester: 'Ross Woodhams',
+    Requestee: 'Raghnall Reynell',
+    Course: 'CS 149',
+    Shift: 'Wednesday, March 13, 2019 4:00pm-6:00pm',
+    Status: 'Approved',
+    description: 'This request has been approved'
+  }, {
+    Requester: 'Shandar Hathaway',
+    Requestee: 'Sebastián Perrault',
+    Course: 'CS 159',
+    Shift: 'Sunday March 16, 2019 1:00pm-3:00pm',
+    Status: 'Pending',
+    description: 'Request is still waiting for approval'
+  }, {
+    Requester: 'Ali Tod',
+    Requestee: 'Alvis Borislavov',
+    Course: 'CS 159',
+    Shift: 'Tuesday March 19, 2019, 6:00pm-8:00pm',
+    Status: 'Pending',
+    description: 'Request is still waiting for approval'
+  }
+];
 
 @Component({
   selector: 'app-admin',
@@ -11,8 +46,8 @@ import {SelectionModel} from '@angular/cdk/collections';
    trigger('detailExpand', [
      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
      state('expanded', style({height: '*'})),
-     transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
-   ]),
+     transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
+   ])
  ],
 })
 export class AdminComponent implements OnInit {
@@ -21,6 +56,7 @@ export class AdminComponent implements OnInit {
   dataSource =   new MatTableDataSource<FakeData>(FAKE_DATA);
   selection = new SelectionModel<FakeData>(true, []);
   expandedElement: FakeData | null;
+  SemesterSel:boolean = false;
 
   isAllSelected() {
    const numSelected = this.selection.selected.length;
@@ -47,36 +83,4 @@ export class AdminComponent implements OnInit {
     this.dataSource.sort = this.sort;
   }
 
-  export interface FakeData {
-    Requester: string;
-    Requestee: string;
-    Course: string;
-    Shift: string;
-    Status: string;
-    description: string;
-  }
-
-  const FAKE_DATA: FakeData[] = [
-    {
-      Requester: 'Ross Woodhams',
-      Requestee: 'Raghnall Reynell',
-      Course: 'CS 149',
-      Shift: 'Wednesday, March 13, 2019 4:00pm-6:00pm',
-      Status: 'Approved',
-      description: 'This request has been approved'
-    }, {
-      Requester: 'Shandar Hathaway',
-      Requestee: 'Sebastián Perrault',
-      Course: 'CS 159',
-      Shift: 'Sunday March 16, 2019 1:00pm-3:00pm',
-      Status: 'Pending',
-      description: `Request is still waiting for approval`
-    }, {
-      Requester: 'Ali Tod',
-      Requestee: 'Alvis Borislavov',
-      Course: 'CS 159',
-      Shift: 'Tuesday March 19, 2019, 6:00pm-8:00pm',
-      Status: 'Pending',
-      description: `Request is still waiting for approval`
-    };
-  ];
+}
