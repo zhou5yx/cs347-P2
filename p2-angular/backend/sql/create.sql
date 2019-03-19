420034ae93f5d812b158b2a3787a3951447851cc
zhou5yx <zhou5yx@dukes.jmu.edu>
2019-03-19 13:54:04 -0400

    test file updates

--- p2-angular/backend/sql/create.sql
+++ p2-angular/backend/sql/create.sql
@@ -1,6 +1,8 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS course;
 DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS test;
+
 
 CREATE TABLE IF NOT EXISTS course (id int NOT NULL AUTO_INCREMENT, name varchar(5), PRIMARY KEY (id));
 CREATE TABLE IF NOT EXISTS user (
@@ -17,3 +19,11 @@ CREATE TABLE IF NOT EXISTS role (
   role_name varchar(255),
   PRIMARY KEY (id)
 );
+
+CREATE TABLE IF NOT EXISTS test (
+   id int NOT NULL AUTO_INCREMENT,
+   PRIMARY KEY(id)
+);
+
+INSERT INTO test VALUES ('1');
+SELECT id from test;
--- p2-angular/test.js
+++ p2-angular/test.js
@@ -1,11 +1,24 @@
-var execsql = require('execsql'),
+/**var execsql = require('execsql'),
     dbConfig = {
         host: 'localhost',
         user: 'root',
-        password: 'root'
+        password: 'password'
     },
-    sql = 'use p2;';
+    sql = 'use p2;',
+    sqlFile = './backend/sql/create.sql';
+
 execsql.config(dbConfig)
-    .exec(sql).execFile('./backend/sql/create.sql', (err, results) => {
+    .exec(sql, function(err,results){
+      console.log(err);
+    })
+    .execFile(sqlFile, function(err, results){
+        console.log(err);
         console.log(results);
-    }).end();
+    }).end;**/
+
+const cp = require('child_process');
+
+cp.exec('mysql --user=root --password=password < use p2 < ./backend/sql/create.sql', (error, stdout, stderr) => {
+         (error) throw error;
+        console.log(`stdout: ${}`);
+});
