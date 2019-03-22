806f9007db5897949b9b5cd9511a38adfc70abc7
Brendan Armani <brendanarmani@gmail.com>
2019-03-22 15:58:55 -0400

    updates to db

--- p2-angular/backend/sql/create.sql
+++ p2-angular/backend/sql/create.sql
@@ -2,14 +2,28 @@ USE p2;
 
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS course;
+DROP TABLE IF EXISTS semester;
 DROP TABLE IF EXISTS role;
 DROP TABLE IF EXISTS location;
 DROP TABLE IF EXISTS event;
+DROP TABLE IF EXISTS session;
+DROP TABLE IF EXISTS session_users;
+DROP TABLE IF EXISTS session_questions;
+DROP TABLE IF EXISTS question;
+DROP TABLE IF EXISTS announcement;
 
 
 CREATE TABLE IF NOT EXISTS course (
   id int NOT NULL,
   name varchar(5),
+  semester_id int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS semester (
+  id int NOT NULL AUTO_INCREMENT,
+  season varchar(10),
+  year int,
   PRIMARY KEY (id)
 );
 
@@ -21,6 +35,7 @@ CREATE TABLE IF NOT EXISTS user (
   lastname varchar(255),
   role_id int,
   course_id int,
+  monthly_hours int,
   PRIMARY KEY (id)
 );
 
@@ -39,10 +54,57 @@ CREATE TABLE IF NOT EXISTS location (
 CREATE TABLE IF NOT EXISTS event (
   id int NOT NULL AUTO_INCREMENT,
   user_id int,
+  requestee int,
   type varchar(10),
   start_date datetime,
   end_date datetime,
   course_id int,
   location int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS session (
+  id int NOT NULL AUTO_INCREMENT,
+  code varchar(255),
+  start_date datetime,
+  end_date datetime,
+  course_id int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS session_users (
+  id int NOT NULL AUTO_INCREMENT,
+  session_id int,
+  user_id int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS session_questions (
+  id int NOT NULL AUTO_INCREMENT,
+  user_id int,
+  session_id int,
+  question_id int,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS question (
+  id int NOT NULL AUTO_INCREMENT,
+  session_id int,
+  title varchar(255),
+  description varchar(1000),
+  ta_answer varchar(1000),
+  student_answer varchar(1000),
+  status varchar(20),
+  votes int,
+  timestamp datetime,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS announcement (
+  id int NOT NULL AUTO_INCREMENT,
+  session_id int,
+  title varchar(255),
+  description varchar(1000),
+  user_id int,
   PRIMARY KEY (id)
 );
--- p2-angular/backend/sql/seed.sql
+++ p2-angular/backend/sql/seed.sql
@@ -1,13 +1,13 @@
 USE p2;
 
-INSERT INTO course VALUES (149, 'CS149');
-INSERT INTO course VALUES (159, 'CS159');
+INSERT INTO course VALUES (149, 'CS149', 1);
+INSERT INTO course VALUES (159, 'CS159', 1);
 
 
-INSERT INTO user VALUES (id, 'meatballboy28', 'spaghetti', 'Nicholas', 'Meatball', 1, 149);
-INSERT INTO user VALUES (id, 'arnolde', 'password', 'Arnold', 'Schwarzenboy', 2, 159);
-INSERT INTO user VALUES (id, 'johnys', 'nicetomeetyou', 'Johnny', 'Jarganfargangas', 1, 149);
-INSERT INTO user VALUES (id, 'happyfeet99', 'penguin', 'Catalina', 'Happyfeetpenguingirl III', 1, 159);
+INSERT INTO user VALUES (id, 'meatballboy28', 'spaghetti', 'Nicholas', 'Meatball', 1, 149, 40);
+INSERT INTO user VALUES (id, 'arnolde', 'password', 'Arnold', 'Schwarzenboy', 2, 159, NULL);
+INSERT INTO user VALUES (id, 'johnys', 'nicetomeetyou', 'Johnny', 'Jarganfargangas', 1, 149, 10);
+INSERT INTO user VALUES (id, 'happyfeet99', 'penguin', 'Catalina', 'Happyfeetpenguingirl III', 1, 159, 15);
 
 
 INSERT INTO location VALUES (143, 'Linux Lab');
@@ -19,16 +19,16 @@ INSERT INTO role VALUES (id, 'ta');
 INSERT INTO role VALUES (id, 'student');
 INSERT INTO role VALUES (id, 'admin');
 
-INSERT INTO event VALUES (id,1,'shift','2019-03-16 17:00:00','2019-03-16 19:00:00',149,250);
-INSERT INTO event VALUES (id,1,'shift','2019-03-19 17:00:00','2019-03-19 19:00:00',149,250);
-INSERT INTO event VALUES (id,1,'shift','2019-03-05 16:00:00','2019-03-05 23:00:00',159,250);
-INSERT INTO event VALUES (id,1,'cover','2019-03-26 17:00:00','2019-03-26 19:00:00',149,250);
-INSERT INTO event VALUES (id,1,'cover','2019-03-18 21:00:00','2019-03-18 23:00:00',159,250);
-INSERT INTO event VALUES (id,1,'shift','2019-03-29 20:00:00','2019-03-29 23:00:00',149,250);
-INSERT INTO event VALUES (id,1,'cover','2019-03-10 19:00:00','2019-03-10 23:00:00',159,250);
-INSERT INTO event VALUES (id,1,'shift','2019-03-16 19:00:00','2019-03-16 21:00:00',149,250);
-INSERT INTO event VALUES (id,1,'shift','2019-03-06 17:00:00','2019-03-06 19:00:00',159,250);
-INSERT INTO event VALUES (id,1,'shift','2019-03-09 17:00:00','2019-03-09 19:00:00',149,250);
-INSERT INTO event VALUES (id,1,'shift','2019-04-14 17:00:00','2019-04-14 19:00:00',159,250);
-INSERT INTO event VALUES (id,1,'shift','2019-04-06 12:00:00','2019-04-06 23:00:00',149,250);
-INSERT INTO event VALUES (id,1,'shift','2019-05-12 17:00:00','2019-05-12 19:00:00',159,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-03-16 17:00:00','2019-03-16 19:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-03-19 17:00:00','2019-03-19 19:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-03-05 16:00:00','2019-03-05 23:00:00',159,250);
+INSERT INTO event VALUES (id, 1, NULL, 'cover','2019-03-26 17:00:00','2019-03-26 19:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL, 'cover','2019-03-18 21:00:00','2019-03-18 23:00:00',159,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-03-29 20:00:00','2019-03-29 23:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL, 'cover','2019-03-10 19:00:00','2019-03-10 23:00:00',159,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-03-16 19:00:00','2019-03-16 21:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-03-06 17:00:00','2019-03-06 19:00:00',159,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-03-09 17:00:00','2019-03-09 19:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-04-14 17:00:00','2019-04-14 19:00:00',159,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-04-06 12:00:00','2019-04-06 23:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL, 'shift','2019-05-12 17:00:00','2019-05-12 19:00:00',159,250);
