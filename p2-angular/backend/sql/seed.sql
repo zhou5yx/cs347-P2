6a2ca02750c678efd8cd62341b0f95059c864af8
rain zhou <zhou5yx@dukes.jmu.edu>
2019-04-01 13:42:06 -0400

    update db

--- p2-angular/backend/sql/seed.sql
+++ p2-angular/backend/sql/seed.sql
@@ -7,12 +7,11 @@ INSERT INTO course VALUES (159, 'CS159', 1);
 INSERT INTO semester VALUES(id, 'Fall', 2019);
 INSERT INTO semester VALUES(id, 'Spring', 2019);
 
-
-INSERT INTO user VALUES (id, 'meatballboy28', 'spaghetti', 'Nicholas', 'Meatball', 1, 159, 40);
+/**
 INSERT INTO user VALUES (id, 'arnolde', 'password', 'Arnold', 'Schwarzenboy', 2, 159, NULL);
 INSERT INTO user VALUES (id, 'johnys', 'nicetomeetyou', 'Johnny', 'Jarganfargangas', 1, 149, 10);
 INSERT INTO user VALUES (id, 'happyfeet99', 'penguin', 'Catalina', 'Happyfeetpenguingirl III', 1, 159, 15);
-
+**/
 
 INSERT INTO role VALUES (id, 'ta');
 INSERT INTO role VALUES (id, 'student');
@@ -24,22 +23,22 @@ INSERT INTO location VALUES (250, 'Linux Lab');
 INSERT INTO location VALUES (252, 'Mac Lab');
 
 
-INSERT INTO event VALUES (id, 3, NULL,NULL, 'shift','2019-03-16 17:00:00','2019-03-16 19:00:00',149,250);
-INSERT INTO event VALUES (id, 3, NULL,NULL, 'shift','2019-03-19 17:00:00','2019-03-19 19:00:00',149,250);
-INSERT INTO event VALUES (id, 1, NULL,NULL, 'shift','2019-03-05 16:00:00','2019-03-05 23:00:00',159,250);
-INSERT INTO event VALUES (id, 1, 4,NULL, 'cover','2019-03-26 17:00:00','2019-03-26 19:00:00',159,250);
-INSERT INTO event VALUES (id, 1, 4, NULL, 'cover','2019-03-18 21:00:00','2019-03-18 23:00:00',159,250);
-INSERT INTO event VALUES (id, 1, NULL, NULL, 'shift','2019-03-29 20:00:00','2019-03-29 23:00:00',159,250);
-INSERT INTO event VALUES (id, 1, 4, NULL, 'cover','2019-03-10 19:00:00','2019-03-10 23:00:00',159,250);
-INSERT INTO event VALUES (id, 3, NULL, NULL, 'shift','2019-03-16 19:00:00','2019-03-16 21:00:00',149,250);
-INSERT INTO event VALUES (id, 1, NULL, NULL, 'shift','2019-03-06 17:00:00','2019-03-06 19:00:00',159,250);
-INSERT INTO event VALUES (id, 3, NULL, NULL, 'shift','2019-03-09 17:00:00','2019-03-09 19:00:00',149,250);
+INSERT INTO event VALUES (id, 3, NULL,NULL, 'shift','2019-04-16 17:00:00','2019-03-16 19:00:00',149,250);
+INSERT INTO event VALUES (id, 3, NULL,NULL, 'shift','2019-04-19 17:00:00','2019-03-19 19:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL,NULL, 'shift','2019-04-05 16:00:00','2019-03-05 23:00:00',159,250);
+INSERT INTO event VALUES (id, 1, 4,NULL, 'cover','2019-04-26 17:00:00','2019-03-26 19:00:00',159,250);
+INSERT INTO event VALUES (id, 1, 4, NULL, 'cover','2019-04-18 21:00:00','2019-03-18 23:00:00',159,250);
+INSERT INTO event VALUES (id, 1, NULL, NULL, 'shift','2019-04-29 20:00:00','2019-03-29 23:00:00',159,250);
+INSERT INTO event VALUES (id, 1, 4, NULL, 'cover','2019-04-10 19:00:00','2019-03-10 23:00:00',159,250);
+INSERT INTO event VALUES (id, 3, NULL, NULL, 'shift','2019-04-16 19:00:00','2019-03-16 21:00:00',149,250);
+INSERT INTO event VALUES (id, 1, NULL, NULL, 'shift','2019-04-06 17:00:00','2019-03-06 19:00:00',159,250);
+INSERT INTO event VALUES (id, 3, NULL, NULL, 'shift','2019-04-09 17:00:00','2019-03-09 19:00:00',149,250);
 INSERT INTO event VALUES (id, 1, NULL, NULL, 'shift','2019-04-14 17:00:00','2019-04-14 19:00:00',159,250);
 INSERT INTO event VALUES (id, 3, NULL, NULL, 'shift','2019-04-06 12:00:00','2019-04-06 23:00:00',149,250);
-INSERT INTO event VALUES (id, 1, NULL, NULL, 'shift','2019-05-12 17:00:00','2019-05-12 19:00:00',159,250);
-INSERT INTO event VALUES (id, 4, 1, NULL, 'shift','2019-03-10 19:00:00','2019-03-10 23:00:00',159,250);
-INSERT INTO event VALUES (id, 4, 1, NULL, 'shift','2019-03-18 21:00:00','2019-03-18 23:00:00',159,250);
-INSERT INTO event VALUES (id, 4, 1,NULL, 'shift','2019-03-26 17:00:00','2019-03-26 19:00:00',159,250);
+INSERT INTO event VALUES (id, 1, NULL, NULL, 'shift','2019-04-12 17:00:00','2019-05-12 19:00:00',159,250);
+INSERT INTO event VALUES (id, 4, 1, NULL, 'shift','2019-04-10 19:00:00','2019-03-10 23:00:00',159,250);
+INSERT INTO event VALUES (id, 4, 1, NULL, 'shift','2019-04-18 21:00:00','2019-03-18 23:00:00',159,250);
+INSERT INTO event VALUES (id, 4, 1,NULL, 'shift','2019-04-26 17:00:00','2019-03-26 19:00:00',159,250);
 
 UPDATE event SET requester=user_id WHERE type = 'shift';
 
